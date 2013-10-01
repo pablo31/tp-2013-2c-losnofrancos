83a8d12b0a490bccfb584109f031429739867a02
pablo31 <pablo.santiago.fernandez@gmail.com>
2013-10-01 14:29:25 -0300

    Proyecto Personaje

---
+++ personaje/personaje.c
@@ -0,0 +1,287 @@
+/*
+ * personaje.c
+ *
+ *  Created on: Sep 25, 2013
+ *      Author: pablo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <libs/common/string.h> // verificar este paht!!!!
+#include "personaje.h"
+#include "mensaje_personaje.c"  //pablo que onda con esta...
+
+bool verificar_argumentos(int argc, char* argv[]);
+sig_atomic_t reiniciar_nivel = 0;
+sig_atomic_t reiniciar_flan = 0;
+
+int main(int argc, char* argv[]) {
+
+	if (!verificar_argumentos(argc, argv)) {
+		return EXIT_FAILURE;
+	}
+
+	//personaje create levanta archivo de configuracion
+	t_personaje* self = personaje_create(argv[1]);
+
+	if (self == NULL ) {
+		return EXIT_FAILURE;
+	}
+
+
+	log_debug(self->logger, "Personaje %s creado", self->nombre);
+
+
+	//se verifican las señales
+	void sigterm_handler(int signum) {
+		morir(self, "Muerte por señal");
+	}
+
+	//estoy hablando de faso XD XD XD
+	void sigusr1_handler(int signum) {
+			comer_honguito_verde(self);
+	}
+	//------------fin de señales
+
+	struct sigaction sigterm_action;
+
+	sigterm_action.sa_handler = sigterm_handler;
+	sigemptyset(&sigterm_action.sa_mask);
+	if (sigaction(SIGTERM, &sigterm_action, NULL ) == -1) {
+		log_error(self->logger, "Error al querer setear la señal SIGTERM");
+		return EXIT_FAILURE;
+	}
+
+	struct sigaction sigusr1_action;
+
+	sigusr1_action.sa_handler = sigusr1_handler;
+	sigusr1_action.sa_flags = SA_RESTART;
+	sigemptyset(&sigusr1_action.sa_mask);
+	if (sigaction(SIGUSR1, &sigusr1_action, NULL ) == -1) {
+		log_error(self->logger, "Error al querer setear la señal SIGUSR1");
+		return EXIT_FAILURE;
+	}
+
+
+
+	// esto no llegue...  =(
+	if (!personaje_conectar_a_nivel(self)) {
+		personaje_destroy(self);
+		return EXIT_FAILURE;
+	}
+
+	if (!personaje_conectar_a_planificador(self)) {
+		personaje_destroy(self);
+		return EXIT_FAILURE;
+	}
+
+
+	personaje_destroy(self);
+
+	return EXIT_SUCCESS;
+}
+
+
+void morir(t_personaje* self, char* motivo) {
+	log_info(self->logger, "Personaje: %s", motivo);
+	if (self->vidas > 0) {
+		self->vidas--;
+		reiniciar_nivel = 1;
+	} else {
+		self->vidas = self->vidas_iniciales;
+		reiniciar_flan = 1;
+		log_info(self->logger, "Personaje %s: Reiniciando vidas", self->nombre);
+	}
+	log_info(self->logger, "Personaje %s: Ahora me quedan %d vidas",
+			self->nombre, self->vidas);
+}
+
+
+
+void comer_honguito_verde(t_personaje* self) {
+	log_info(self->logger,
+			"Personaje %s: Llegó un honguito de esos que pegan ;)",
+			self->nombre);
+	self->vidas++;
+	log_info(self->logger, "Personaje %s: Ahora tengo %d vidas", self->nombre,
+			self->vidas);
+}
+
+bool verificar_argumentos(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("Error en la cantidad de argumentos.\n");
+		return false;
+	}
+	return true;
+}
+
+t_personaje* personaje_create(char* config_path) {
+	t_personaje* new = malloc(sizeof(t_personaje));
+	t_config* config = config_create(config_path); //commons... andres para nivel es igual...
+
+	new->nombre = string_duplicate(config_get_string_value(config, "nombre"));
+
+	char* s = string_duplicate(config_get_string_value(config, "simbolo"));
+	new->simbolo = s[0];
+
+	new->plan_de_niveles = config_get_array_value(config, "planDeNiveles");
+	new->objetivos = _personaje_load_objetivos(config, new->plan_de_niveles);
+	new->vidas = config_get_int_value(config, "vidas");
+	new->orquestador_info = connection_create(
+			config_get_string_value(config, "orquestador"));
+
+	void morir(char* mensaje) {
+		config_destroy(config);
+		free(s);
+		personaje_destroy(new);
+		printf("Error en el archivo de configuración: %s\n", mensaje);
+	}
+
+	if (!config_has_property(config, "puerto")) {
+		morir("Falta el puerto");
+		return NULL ;
+	}
+	new->puerto = config_get_int_value(config, "puerto");
+
+	char* log_file = "personaje.log";
+	char* log_level = "INFO";
+	if (config_has_property(config, "logFile")) {
+		log_file = string_duplicate(config_get_string_value(config, "logFile"));
+	}
+	if (config_has_property(config, "logLevel")) {
+		log_level = string_duplicate(
+				config_get_string_value(config, "logLevel"));
+	}
+	new->logger = log_create(log_file, "Personaje", true,
+			log_level_from_string(log_level));
+	config_destroy(config);
+
+	new->socket_orquestador = NULL;
+	new->nivel_actual = NULL;
+	new->posicion = NULL;
+	new->posicion_objetivo = NULL;
+	new->nivel_finalizado = false;
+	new->nivel_actual_index = 0;
+	new->vidas_iniciales = new->vidas;
+	new->objetivos_array = NULL;
+	new->objetivo_actual = NULL;
+	new->objetivo_actual_index = 0;
+	new->is_blocked = false;
+
+	free(s);
+	free(log_file);
+	free(log_level);
+	return new;
+}
+
+
+
+void personaje_destroy(t_personaje* self) {
+	free(self->nombre);
+	array_destroy(self->plan_de_niveles);
+	dictionary_destroy_and_destroy_elements(self->objetivos,
+			(void*) array_destroy);
+	connection_destroy(self->orquestador_info);
+	log_destroy(self->logger);
+	if (self->socket_orquestador != NULL ) {
+		if (self->socket_orquestador->serv_socket != NULL ) {
+			sockets_destroy(self->socket_orquestador->serv_socket);
+		}
+		sockets_destroyClient(self->socket_orquestador);
+	}
+	if (self->nivel_actual != NULL ) {
+		personaje_nivel_destroy(self->nivel_actual);
+	}
+	if (self->posicion != NULL ) {
+		posicion_destroy(self->posicion);
+	}
+
+	if (self->posicion_objetivo != NULL ) {
+		posicion_destroy(self->posicion_objetivo);
+	}
+
+	if (self->objetivo_actual != NULL ) {
+		free(self->objetivo_actual);
+	}
+
+	free(self);
+}
+
+
+bool personaje_conectar_a_orquestador(t_personaje* self) {
+
+	self->socket_orquestador = sockets_conectar_a_servidor(NULL, self->puerto,
+			self->orquestador_info->ip, self->orquestador_info->puerto,
+			self->logger, M_HANDSHAKE_PERSONAJE, PERSONAJE_HANDSHAKE,
+			HANDSHAKE_SUCCESS, "Orquestador");
+
+	if (self->socket_orquestador == NULL ) {
+		return false;
+	}
+
+	t_mensaje* mensaje = mensaje_recibir(self->socket_orquestador);
+
+	if (mensaje == NULL ) {
+		log_error(self->logger,
+				"Personaje %s: El orquestador se ha desconectado.",
+				self->nombre);
+		return false;
+	}
+
+	if (mensaje->type != M_GET_SYMBOL_PERSONAJE_REQUEST) {
+		mensaje_destroy(mensaje);
+		return false;
+	}
+
+	mensaje_destroy(mensaje);
+
+	char* simbolo = string_from_format("%c", self->simbolo);
+
+	mensaje_create_and_send(M_GET_SYMBOL_PERSONAJE_RESPONSE,
+			string_duplicate(simbolo), strlen(simbolo) + 1,
+			self->socket_orquestador);
+	free(simbolo);
+
+	return true;
+}
+
+
+bool personaje_conectar_a_planificador(t_personaje* self) {
+	self->nivel_actual->socket_planificador = sockets_conectar_a_servidor(NULL,
+			self->puerto, self->nivel_actual->planificador->ip,
+			self->nivel_actual->planificador->puerto, self->logger,
+			M_HANDSHAKE_PERSONAJE, PERSONAJE_HANDSHAKE, HANDSHAKE_SUCCESS,
+			"Planificador");
+
+	if (self->nivel_actual->socket_planificador == NULL ) {
+		return false;
+	}
+
+	t_mensaje* mensaje = mensaje_recibir(
+			self->nivel_actual->socket_planificador);
+
+	if (mensaje == NULL ) {
+		log_error(self->logger,
+				"Personaje %s: El planificador del nivel %s se ha desconectado.",
+				self->nombre, self->nivel_actual->nombre);
+		return false;
+	}
+
+	if (mensaje->type != M_GET_SYMBOL_PERSONAJE_REQUEST) {
+		mensaje_destroy(mensaje);
+		return false;
+	}
+
+	mensaje_destroy(mensaje);
+
+	char* simbolo = string_from_format("%c", self->simbolo);
+
+	mensaje_create_and_send(M_GET_SYMBOL_PERSONAJE_RESPONSE,
+			string_duplicate(simbolo), strlen(simbolo) + 1,
+			self->nivel_actual->socket_planificador);
+	free(simbolo);
+
+	return true;
+}
---
+++ personaje/personaje.h
@@ -0,0 +1,70 @@
+/*
+ * personaje.h
+ *
+ *  Created on: Sep 25, 2013
+ *      Author: pablo
+ */
+#include "personaje_structs.h"
+
+
+#ifndef PERSONAJE_H_
+#define PERSONAJE_H_
+
+
+typedef struct {
+	char* nombre;
+	t_connection_info* nivel;
+	t_connection_info* planificador;
+	t_socket_client* socket_nivel;
+	t_socket_client* socket_planificador;
+} t_personaje_nivel;
+
+typedef struct {
+	char* nombre;
+	char simbolo;
+	char** plan_de_niveles;  // la idea es usar lista de lista,
+	t_dictionary* objetivos; //diccionario porque son fijos
+	int vidas;
+	t_connection_info* orquestador_info;   //pablo en algun lado tenes que
+	t_socket_client* socket_orquestador;   //tener estructuras como estas  =)
+	int puerto;
+	t_log* logger;
+	t_personaje_nivel* nivel_actual;
+	t_posicion* posicion;
+	t_posicion* posicion_objetivo;
+	bool nivel_finalizado;
+	int nivel_actual_index;
+	int vidas_iniciales;
+	char** objetivos_array;
+	char* objetivo_actual;
+	int objetivo_actual_index;
+	bool is_blocked;
+} t_personaje;
+
+
+
+t_personaje* personaje_create(char* config_path);  //ok
+void personaje_destroy(t_personaje* self);         //ok
+t_personaje_nivel* personaje_nivel_create(char* nombre_nivel);
+void personaje_nivel_destroy(t_personaje_nivel* self);
+t_dictionary* _personaje_load_objetivos(t_config* config,
+		char** plan_de_niveles);
+
+//creo que es toda la funcionalidad de personaje, no se si me olvido de algo
+bool personaje_get_info_nivel(t_personaje* self);
+bool personaje_conectar_a_orquestador(t_personaje* self);
+bool personaje_conectar_a_nivel(t_personaje* self);
+bool personaje_conectar_a_planificador(t_personaje* self);
+bool personaje_jugar_nivel(t_personaje* self);
+t_posicion* pedir_posicion_objetivo(t_personaje* self, char* objetivo);
+bool realizar_movimiento(t_personaje* self);
+bool mover_en_nivel(t_personaje* self);
+bool finalizar_turno(t_personaje* self);
+t_mensaje* solicitar_recurso(t_personaje* self);
+void finalizar_nivel(t_personaje* self);
+void personaje_avisar_fin_de_nivel(t_personaje* self);
+void morir(t_personaje* self, char* motivo);
+void avisar_muerte_a_nivel(t_personaje* self);
+
+
+#endif /* PERSONAJE_H_ */
