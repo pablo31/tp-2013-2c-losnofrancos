d34a189049b1c7c4c904a42c0d40a91374aaf345
Andres Nagel <montsegur87@gmail.com>
2013-10-20 04:08:10 -0300

    Leyendo el header del archivo grasa via mmap y fuse

--- filesystem/filesystem.c
+++ filesystem/filesystem.c
@@ -1,11 +1,27 @@
-#include <stddef.h>
+#include <string.h>
 #include <stdlib.h>
 #include <fuse.h>
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/mman.h>
 #include "grasa.h"
+#include <stdbool.h>
+#include "filesystem_operations.h"
+#include "../libs/logger/logger.h"
+#include "../libs/common/string.h"
+
+#define TAMANIO_IDENTIFICADOR 5
+#define TAMANIO_VERSION       4
+#define TAMANIO_PTR_BITMAP    sizeof(ptrGBloque) 
+#define TAMANIO_CANT_BITMAPS  4
+#define TAMANIO_RELLENO       4073  
+
+#define FIN_IDENTIFICADOR    TAMANIO_IDENTIFICADOR
+#define FIN_VERSION          FIN_IDENTIFICADOR + TAMANIO_VERSION 
+#define FIN_PUNTERO_BITMAP   FIN_VERSION + TAMANIO_PTR_BITMAP
+#define FIN_CANTIDAD_BITMAPS TAMANIO_PTR_BITMAP + TAMANIO_CANT_BITMAPS
+#define FIN_RELLENO          TAMANIO_CANTIDAD_BITMAPS + TAMANIO_RELLENO
 
 struct t_runtime_options {
 	//char* welcome_msg;
@@ -16,94 +32,99 @@ enum {
 	KEY_HELP,
 };
 
-/*
- * Esta estructura es utilizada para decirle a la biblioteca de FUSE que
- * parametro puede recibir y donde tiene que guardar el valor de estos
- */
-static struct fuse_opt fuse_options[] = {
-		// Estos son parametros por defecto que ya tiene FUSE
-		FUSE_OPT_KEY("-V", KEY_VERSION),
-		FUSE_OPT_KEY("--version", KEY_VERSION),
-		FUSE_OPT_KEY("-h", KEY_HELP),
-		FUSE_OPT_KEY("--help", KEY_HELP),
-		FUSE_OPT_END,
-};
+static int fd;//file descriptor del archivo de grasa
+static char* mapped_header; //el array de chars que contiene el archivo via FUSE
 
-/*
-Read data from an open file
+static struct grasa_header_t* cargar_header(const char* filename){
+	fd = open(filename, O_RDONLY);
 
-Read should return exactly the number of bytes requested except on EOF or error, otherwise the rest of the data will be substituted 
-with zeroes. An exception to this is when the 'direct_io' mount option is specified, in which case the return value of the read system 
-call will reflect the return value of this operation.
-*/
-static int fs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
-	return 1;
+	struct stat status;
+    fstat (fd, &status);
+ 	mapped_header = mmap(NULL, status.st_size, PROT_READ, MAP_PRIVATE, fd, 0);	
+
+ 	if (mapped_header == MAP_FAILED){
+ 		printf("Error al iniciar mmap con el archivo %s. '%s' \n", filename, strerror(errno));
+ 		exit(EXIT_FAILURE); //terminamos forzosamente el proceso.
+ 	}
+
+ 	struct grasa_header_t* header = malloc(sizeof(struct grasa_header_t));
+
+	char* identificador = string_substring(mapped_header, 0 , FIN_IDENTIFICADOR);
+	strcpy((char*)header->grasa, identificador);
+	free(identificador);
+
+	char version_temp[TAMANIO_VERSION];  
+	int i;
+	for (i = FIN_IDENTIFICADOR; i < FIN_VERSION; ++i)	
+		version_temp[i - FIN_IDENTIFICADOR] =  '0' + mapped_header[i];
+	
+	header->version = strtol(version_temp,NULL,10);
+	
+
+	//printf("%i %lu\n" , FIN_VERSION, FIN_PUNTERO_BITMAP );
+	char ptrBloque[TAMANIO_PTR_BITMAP];
+	for (i = FIN_VERSION; i < FIN_PUNTERO_BITMAP; ++i)
+		ptrBloque[i - FIN_VERSION] =  '0' + mapped_header[i];
+
+	header->blk_bitmap = strtol(ptrBloque,NULL,10);
+	
+
+	char tamanio_bitmap[TAMANIO_CANT_BITMAPS];
+	for (i = TAMANIO_PTR_BITMAP; i < FIN_CANTIDAD_BITMAPS; ++i)	
+		tamanio_bitmap[i - TAMANIO_PTR_BITMAP] =  '0' + mapped_header[i];
+
+	header->size_bitmap = strtol(tamanio_bitmap,NULL,10);
+	
+
+	char* tamanio_relleno = string_substring(mapped_header, FIN_CANTIDAD_BITMAPS , TAMANIO_RELLENO);;	
+	strcpy((char*)header->padding, tamanio_relleno);
+	
+	free(tamanio_relleno);
+
+ 	return header;
 }
 
+
+int main(int argc, char *argv[]){
+ 	struct grasa_header_t* header = cargar_header("disco.bin"); 
+ 	
+
+	printf("Identificador\t%s\n", header->grasa);
+ 	printf("Versión\t%i\n", header->version);
+ 	printf("Bloque de inicio del bitmap\t%i\n", header->blk_bitmap);	
+	printf("Tamaño del Bitmap (en bloques)\t%i\n", header->size_bitmap);
+	printf("Relleno lo leo pero no le doy pelota\n");
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
-Create a directory
-
-Note that the mode argument may not have the type specification bits set, i.e. S_ISDIR(mode) can be false. To obtain the correct 
-directory type bits use mode|S_IFDIR
-*/
-static int fs_mkdir(const char *path, mode_t mode){
-	return 1;	
-}
-
-/*
-Create and open a file
-
-If the file does not exist, first create it with the specified mode, and then open it.
-
-If this method is not implemented or under Linux kernel versions earlier than 2.6.15, the mknod() and open() methods will be called instead.
-*/
-static int fs_create(const char *path, mode_t mode, struct fuse_file_info *fi){
-	return 1;	
-}
-
-
-/*
-Open directory
-
-Unless the 'default_permissions' mount option is given, this method should check if opendir is permitted for this directory. Optionally opendir may also return an arbitrary filehandle in the fuse_file_info structure, which will be passed to readdir, closedir and fsyncdir.
-
-*/
-static int fs_opendir(const char *path, struct fuse_file_info *fi){
-	return 1;	
-}
-/*
-Write data to an open file
-
-Write should return exactly the number of bytes requested except on error. An exception to this is when the 'direct_io' mount option is specified (see read operation).
-*/
-static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
-	return 1;
-}
-
-/*
-Remove a file
-*/
-static int fs_unlink(const char *path){
-	//Remove a file
-	return 1;
-}
-
-static int fs_rmdir (const char *path){
-	return 1;
-}
-
-static struct fuse_operations fuse_operations = {
-		.read = fs_read,
-		.mkdir = fs_mkdir,
-		.create = fs_create,
-		.opendir = fs_opendir,
-		.write = fs_write,
-		.unlink = fs_unlink,
-		.rmdir = fs_rmdir,
-		
-};
-#define CUSTOM_FUSE_OPT_KEY(t, p, v) { t, offsetof(struct t_runtime_options, p), v }
-
 // Dentro de los argumentos que recibe nuestro programa obligatoriamente
 // debe estar el path al directorio donde vamos a montar nuestro FS
 int main(int argc, char *argv[]) {
@@ -114,7 +135,6 @@ int main(int argc, char *argv[]) {
 
 	// Esta funcion de FUSE lee los parametros recibidos y los intepreta
 	if (fuse_opt_parse(&args, &runtime_options, fuse_options, NULL) == -1){
-		/** error parsing options */
 		perror("Invalid arguments!");
 		return EXIT_FAILURE;
 	}
@@ -124,4 +144,28 @@ int main(int argc, char *argv[]) {
 	// en varios threads
 	return fuse_main(args.argc, args.argv, &fuse_operations, NULL);
 }
+*/
+/*
+ * Esta estructura es utilizada para decirle a la biblioteca de FUSE que
+ * parametro puede recibir y donde tiene que guardar el valor de estos
+ */
+/*
+static struct fuse_opt fuse_options[] = {
+		// Estos son parametros por defecto que ya tiene FUSE
+		FUSE_OPT_KEY("-V", KEY_VERSION),
+		FUSE_OPT_KEY("--version", KEY_VERSION),
+		FUSE_OPT_KEY("-h", KEY_HELP),
+		FUSE_OPT_KEY("--help", KEY_HELP),
+		FUSE_OPT_END,
+};
 
+static struct fuse_operations fuse_operations = { //las funciones estan en filesystem_operations.c
+		.read = fs_read,
+		.mkdir = fs_mkdir,
+		.create = fs_create,
+		.opendir = fs_opendir,
+		.write = fs_write,
+		.unlink = fs_unlink,
+		.rmdir = fs_rmdir,		
+};
+*/
---
+++ filesystem/filesystem_operations.c
@@ -0,0 +1,71 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+//#include <fcntl.h>
+#include "grasa.h"
+
+/*
+Read data from an open file
+
+Read should return exactly the number of bytes requested except on EOF or error, otherwise the rest of the data will be substituted 
+with zeroes. An exception to this is when the 'direct_io' mount option is specified, in which case the return value of the read system 
+call will reflect the return value of this operation.
+*/
+int fs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+	return 1;
+}
+
+/*
+Create a directory
+
+Note that the mode argument may not have the type specification bits set, i.e. S_ISDIR(mode) can be false. To obtain the correct 
+directory type bits use mode|S_IFDIR
+*/
+int fs_mkdir(const char *path, mode_t mode){
+	return 1;	
+}
+
+/*
+Create and open a file
+
+If the file does not exist, first create it with the specified mode, and then open it.
+
+If this method is not implemented or under Linux kernel versions earlier than 2.6.15, the mknod() and open() methods will be called instead.
+*/
+int fs_create(const char *path, mode_t mode, struct fuse_file_info *fi){
+	return 1;	
+}
+
+
+/*
+Open directory
+
+Unless the 'default_permissions' mount option is given, this method should check if opendir is permitted for this directory. Optionally opendir may also return an arbitrary filehandle in the fuse_file_info structure, which will be passed to readdir, closedir and fsyncdir.
+
+*/
+int fs_opendir(const char *path, struct fuse_file_info *fi){
+	return 1;	
+}
+/*
+Write data to an open file
+
+Write should return exactly the number of bytes requested except on error. An exception to this is when the 'direct_io' mount option is specified (see read operation).
+*/
+int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+	return 1;
+}
+
+/*
+Remove a file
+*/
+int fs_unlink(const char *path){
+	//Remove a file
+	return 1;
+}
+
+int fs_rmdir (const char *path){
+	return 1;
+}
