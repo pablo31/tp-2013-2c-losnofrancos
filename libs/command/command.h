77d57d5261aef73e855828eedd1f0202ec5a0ed7
pablo31 <pablo.santiago.fernandez@gmail.com>
2013-10-20 14:05:55 -0300

    Ultimos retoques de overload

--- libs/command/command.h
+++ libs/command/command.h
@@ -11,8 +11,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-#include "../variadic.h"
-
 #include "arguments.h"
 
 
@@ -44,11 +42,12 @@ void command_dispose(tad_command* command);
 	void* __r_md_argsptr
 #define UNPACK_ARG(__arg) \
 	__arg = get_next_argument(__r_md_argsptr)
+
+
+#include "../overload.h"
+//overloaded packed-args macro
 #define UNPACK_ARGS(...) \
-	concat_numargs(UNPACK_ARGS, __VA_ARGS__) (__VA_ARGS__)
-
-
-
+	overload(UNPACK_ARGS, __VA_ARGS__)
 //private macros
 #define UNPACK_ARGS1(__arg) \
 	UNPACK_ARG(__arg)
@@ -60,6 +59,10 @@ void command_dispose(tad_command* command);
 	UNPACK_ARG(__arg1); UNPACK_ARG(__arg2); UNPACK_ARG(__arg3); UNPACK_ARG(__arg4)
 #define UNPACK_ARGS5(__arg1, __arg2, __arg3, __arg4, __arg5) \
 	UNPACK_ARG(__arg1); UNPACK_ARG(__arg2); UNPACK_ARG(__arg3); UNPACK_ARG(__arg4); UNPACK_ARG(__arg5)
+
+//#define UNPACK_ARGS(...) concat_hasargs(UNPACK_ARGS, __VA_ARGS__) (__VA_ARGS__)
+//#define UNPACK_ARGS0(...)
+//#define UNPACK_ARGS1(_arg, ...) UNPACK_ARG(__arg); UNPACK_ARGS(__VA_ARGS__)
 
 
 #endif /* COMMAND_H_ */
