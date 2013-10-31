f3ca2987e1c6569dd39e5cda02713ce9b3b52b41
pablo31 <pablo.santiago.fernandez@gmail.com>
2013-10-31 15:36:00 -0300

    Rearmo nivel_ui utilizando una t_list. Cambio en el macro foreach.

--- nivel/nivel_ui.c
+++ nivel/nivel_ui.c
@@ -5,38 +5,33 @@
 #include "nivel.h"
 #include "nivel_ui.h"
 #include "../libs/logger/logger.h"
-#include "../libs/common/config.h"
-#include "../libs/vector/vector2.h"
-#include "../libs/common.h"
 
 static WINDOW * secwin;
 static WINDOW * mainwin;
 static int rows, cols;
-static int inicializado = 0;
+private t_list* items; //list<gui_item>
 
-static void nivel_gui_get_term_size(int * rows, int * cols) {
+
+private void nivel_gui_get_term_size(int as_out rows, int as_out cols){
     struct winsize ws;
 
-    if ( ioctl(0, TIOCGWINSZ, &ws) < 0 ) {
-        perror("couldn't get window size");
-    }
+    if(ioctl(0, TIOCGWINSZ, &ws) < 0)
+    	perror("couldn't get window size");
 
-    *rows = ws.ws_row;
-    *cols = ws.ws_col;
-}
-static int nivel_gui_int_validar_inicializado(void){
-	return inicializado;
-}
-static void nivel_gui_print_perror(const char* message){
-	fprintf(stderr, "%s\n", message);
+    set rows = ws.ws_row;
+    set cols = ws.ws_col;
 }
 
-int nivel_gui_inicializar() {	
-	
-	if (nivel_gui_int_validar_inicializado()){
-		return EXIT_FAILURE;
-	}
+//private void nivel_gui_get_area_nivel(int as_out rows, int as_out cols){
+//	int term_rows;
+//	int term_cols;
+//	nivel_gui_get_term_size(out term_rows, out term_cols);
+//	set rows = term_rows - 4;
+//	set cols = term_cols - 2;
+//}
 
+
+void nivel_gui_inicializar(){
 	mainwin = initscr();
 	keypad(stdscr, TRUE);
 	noecho();
@@ -52,25 +47,11 @@ int nivel_gui_inicializar() {
 	box(secwin, 0, 0);
 	wrefresh(secwin);
 
-	inicializado = 1;
-
-	return EXIT_SUCCESS;
+	items = list_create();
 }
 
 
-int nivel_gui_dibujar(ITEM_NIVEL* items) {
-	if (!nivel_gui_int_validar_inicializado()){
-		nivel_gui_print_perror("nivel_gui_dibujar: Library no inicializada!");
-		return EXIT_FAILURE;
-	}
-
-	if (items == NULL){
-		nivel_gui_print_perror("nivel_gui_dibujar: La lista de items no puede ser NULL");
-		return EXIT_FAILURE;
-	}
-
-
-	ITEM_NIVEL *temp = items;
+void nivel_gui_dibujar() {
 	int i = 0;
 
 	werase(secwin);
@@ -80,176 +61,93 @@ int nivel_gui_dibujar(ITEM_NIVEL* items) {
 	move(rows - 2, 2);
 	printw("Recursos: ");
 
-	while (temp != NULL) {
-		wmove (secwin, temp->posx, temp->posy);
-		if (temp->item_type) {
-			waddch(secwin, temp->id | COLOR_PAIR(3));
+	foreach(item, items, gui_item*){
+		wmove (secwin, item->pos.x, item->pos.y);
+		if (item->item_type) {
+			waddch(secwin, item->id | COLOR_PAIR(3));
 		} else {
-			waddch(secwin, temp->id | COLOR_PAIR(2));
+			waddch(secwin, item->id | COLOR_PAIR(2));
 		}
-		if (temp->item_type) {
+		if (item->item_type) {
 			move(rows - 2, 7 * i + 3 + 9);
-			printw("%c: %d - ", temp->id, temp->quantity);
+			printw("%c: %d - ", item->id, item->quantity);
 			i++;
 		}
-		temp = temp->next;
-
 	}
+
 	wrefresh(secwin);
 	wrefresh(mainwin);
-
-	return EXIT_SUCCESS;
 }
 
-int nivel_gui_terminar() {
-		if (!nivel_gui_int_validar_inicializado()){
-			//tp_logger_error(logger,"nivel_gui_terminar: Library no inicializada!");
-			return EXIT_FAILURE;
-		}
-
-
-		//tp_logger_debug(logger, "Borrando ventanas de Curses.");
-        delwin(mainwin);
-        delwin(secwin);
-        endwin();
-        refresh();
-
-        return EXIT_SUCCESS;
+void nivel_gui_terminar(){
+	delwin(mainwin);
+	delwin(secwin);
+	endwin();
+	refresh();
 }
 
-int nivel_gui_get_area_nivel(int * rows, int * cols) {
-	if (!nivel_gui_int_validar_inicializado()){
-			nivel_gui_print_perror("nivel_gui_get_area_nivel: Library no inicializada!");
-			return EXIT_FAILURE;
-	}
+static void nivel_gui_crear_item(char id, vector2 pos, char tipo, int cant_rec) {
+        alloc(item, gui_item);
 
-	if (rows == NULL || cols == NULL){
-		nivel_gui_print_perror("nivel_gui_get_area_nivel: Ninguno de los argumentos puede ser NULL");
-		return EXIT_FAILURE;
-	}
+        item->id = id;
+        item->pos = pos;
+        item->item_type = tipo;
+        item->quantity = cant_rec;
 
-	nivel_gui_get_term_size(rows, cols);
-	*rows = *rows - 4;
-	*cols = *cols - 2;
-
-	return EXIT_SUCCESS;
+        list_add(items, item);
 }
 
-static void nivel_gui_crear_item(ITEM_NIVEL** ListaItems, char id, int x , int y, char tipo, int cant_rec) {
-        ITEM_NIVEL * temp;
-        temp = malloc(sizeof(ITEM_NIVEL));
-
-        temp->id = id;
-        temp->posx=x;
-        temp->posy=y;
-        temp->item_type = tipo;
-        temp->quantity = cant_rec;
-        temp->next = *ListaItems;
-        *ListaItems = temp;
+void nivel_gui_crear_personaje(char id, vector2 pos) {
+	nivel_gui_crear_item(id, pos, PERSONAJE_ITEM_TYPE, 0);
 }
 
-void nivel_gui_crear_personaje(ITEM_NIVEL** ListaItems, char id, int x , int y) {
-        nivel_gui_crear_item(ListaItems, id, x, y, PERSONAJE_ITEM_TYPE, 0);
+void nivel_gui_crear_caja(tad_caja* c) {
+	nivel_gui_crear_item(c->simbolo, c->pos, RECURSO_ITEM_TYPE, c->instancias);
 }
 
-void nivel_gui_crear_caja(ITEM_NIVEL** ListaItems, tad_caja* c) {
-        nivel_gui_crear_item(ListaItems, c->simbolo, c->pos.x, c->pos.y, RECURSO_ITEM_TYPE, c->instancias);
-}
-
-
-void nivel_gui_crear_enemigo(ITEM_NIVEL** ListaItems, tad_enemigo* enem, int seed) {
+void nivel_gui_crear_enemigo(tad_enemigo* enem, int seed) {
 		//la seed o semilla es un numero para generar valores aleatorios. 
 		//Se lo paso por argumento asi lo modifico en el for anterior, sino me da siempre numeros iguales
 		// le sumo 1 al resultado porque no puede ser 0/0 la posicion.
 		srand (seed);
-		int pos_x = 1 + (rand() % rows);
-		int pos_y = 1 + (rand() % cols);
+		int x = 1 + (rand() % rows);
+		int y = 1 + (rand() % cols);
 
-		enem->pos = vector2_new(pos_x, pos_y);
+		enem->pos = vector2_new(x, y);
 		
-        nivel_gui_crear_item(ListaItems, enem->simbolo, enem->pos.x, enem->pos.y, ENEMIGO_ITEM_TYPE, 1);
+        nivel_gui_crear_item(enem->simbolo, enem->pos, ENEMIGO_ITEM_TYPE, 1);
 }
 
-void nivel_borrar_item(ITEM_NIVEL** ListaItems, char id) {
-        ITEM_NIVEL * temp = *ListaItems;
-        ITEM_NIVEL * oldtemp;
-
-        if ((temp != NULL) && (temp->id == id)) {
-                *ListaItems = (*ListaItems)->next;
-		free(temp);
-        } else {
-                while((temp != NULL) && (temp->id != id)) {
-                        oldtemp = temp;
-                        temp = temp->next;
-                }
-                if ((temp != NULL) && (temp->id == id)) {
-                        oldtemp->next = temp->next;
-			free(temp);
-                }
-        }
-
+void nivel_borrar_item(char id) {
+	bool item_buscado(void* ptr_item){
+		return ((gui_item*)ptr_item)->id == id;
+	}
+	list_remove_by_condition(items, item_buscado);
 }
 
-void nivel_gui_mover_personaje(ITEM_NIVEL* ListaItems, char id, int x, int y) {
-        ITEM_NIVEL * temp;
-        temp = ListaItems;
 
-        while ((temp != NULL) && (temp->id != id)) {
-                temp = temp->next;
-        }
-        if ((temp != NULL) && (temp->id == id)) {
-                temp->posx = x;
-                temp->posy = y;
-        }
+void nivel_gui_mover_item(char id, vector2 new_pos){
+	foreach(item, items, gui_item*)
+		if(item->id == id)
+			item->pos = new_pos;
 }
 
-void nivel_restar_recurso(ITEM_NIVEL* ListaItems, char id) {
-        ITEM_NIVEL * temp;
-        temp = ListaItems;
-
-        while ((temp != NULL) && (temp->id != id)) {
-                temp = temp->next;
-        }
-        if ((temp != NULL) && (temp->id == id)) {
-                if ((temp->item_type) && (temp->quantity > 0)) {
-                		int quantity = temp->quantity;
-                		quantity--;
-                        temp->quantity = quantity;
-                }
-        }
+void nivel_restar_recurso(char id){
+	foreach(item, items, gui_item*)
+		if(item->id == id)
+			item->quantity--;
 }
 
-void cargar_recursos_nivel(tad_nivel* nivel){	
-	ITEM_NIVEL* ListaItems = NULL;
-
-	
-	//int q, p;
-
-	//int x = 1;
-	//int y = 1;
-
-    //nivel_gui_get_area_nivel(&rows, &cols);
-
-	//q = rows;
-	//p = cols;
-
-	//tp_logger_info(logger, "Cargando recursos del nivel en la libreria de Curses.");
-	int cantidad_niveles = list_size(nivel->cajas); 
-	int i;
-	for (i=0; i < cantidad_niveles ; i++){
-		tad_caja* caja = list_get(nivel->cajas,i);
-		nivel_gui_crear_caja(&ListaItems, caja);
+void cargar_recursos_nivel(tad_nivel* nivel){
+	foreach(caja, nivel->cajas, tad_caja*){
+		nivel_gui_crear_caja(caja);
 	}
 
-	int cantidad_enemigos = list_size(nivel->enemigos);
 	int seed = time(null);
-	for (i=0; i < cantidad_enemigos; i++){
-		tad_enemigo* enem = list_get(nivel->enemigos,i);
-		nivel_gui_crear_enemigo(&ListaItems, enem, seed);
+	foreach(enemigo, nivel->enemigos, tad_enemigo*){
+		nivel_gui_crear_enemigo(enemigo, seed);
 		seed++;
 	}
-
 	
-	//tp_logger_info(logger, "Iniciando el dibujado del nivel.");
-	nivel_gui_dibujar(ListaItems);
+	nivel_gui_dibujar();
 }
--- nivel/nivel_ui.h
+++ nivel/nivel_ui.h
@@ -5,34 +5,46 @@
 #define RECURSO_ITEM_TYPE 1
 #define ENEMIGO_ITEM_TYPE 2
 
+#include "../libs/common.h"
+#include "../libs/vector/vector2.h"
+
 #include "nivel.h"
 
-struct item {
+
+class(gui_item){
 	char id;
-	int posx;
-	int posy;
-	char item_type; // PERSONAJE o CAJA_DE_RECURSOS
+	char item_type; //PERSONAJE o CAJA_DE_RECURSOS
 	int quantity;
-	struct item *next;
+	vector2 pos;
 };
 
-typedef struct item ITEM_NIVEL;
+
+
+//struct item {
+//	char id;
+//	int posx;
+//	int posy;
+//	char item_type; //PERSONAJE o CAJA_DE_RECURSOS
+//	int quantity;
+//	struct item *next;
+//};
+//
+//typedef struct item ITEM_NIVEL;
 
 /*
  *  Inicializacion y Destruccion del nivel
  */
 
-int nivel_gui_inicializar();
-int nivel_gui_terminar();
-int nivel_gui_get_area_nivel(int * rows, int * cols);
-int nivel_gui_dibujar(ITEM_NIVEL* items);
+void nivel_gui_inicializar();
+void nivel_gui_terminar();
+void nivel_gui_dibujar();
 
 /*
  *  Acciones sobre recursos y personajes
  */
-void nivel_borrar_item(ITEM_NIVEL** i, char id);
-void nivel_restar_recurso(ITEM_NIVEL* i, char id);
-void nivel_gui_mover_personaje(ITEM_NIVEL* i, char personaje, int x, int y);
-void nivel_gui_crear_personaje(ITEM_NIVEL** i, char id, int x , int y);
-void nivel_gui_crear_caja(ITEM_NIVEL** i, tad_caja* c);
+void nivel_borrar_item(char id);
+void nivel_restar_recurso(char id);
+void nivel_gui_mover_item(char id, vector2 new_pos);
+void nivel_gui_crear_personaje(char id, vector2 pos);
+void nivel_gui_crear_caja(tad_caja* c);
 #endif
