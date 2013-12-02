ffd289ab6e19dda9922316e51caa5fdaa324976e
Andres Nagel <montsegur87@gmail.com>
2013-12-02 17:49:47 -0300

    Cambios en archivo de config de Personaje

--- personaje/personaje_config.c
+++ personaje/personaje_config.c
@@ -24,19 +24,30 @@ t_personaje* personaje_crear(char* config_path){
 	self->simbolo = *config_get_string_value(config, "simbolo");
 
 	//cargamos los datos del logger
-	char* log_file = config_get_string_value(config, "logFile");
-	char* log_level = config_get_string_value(config, "logLevel");
-	logger_initialize(log_file, "personaje.sh", log_level);
+	char* log_file;
+	if(config_has_property(config,"LogFile"))
+		log_file = config_get_string_value(config, "LogFile");
+	else 
+		log_file = string_from_format("%s.log", self->nombre);
+
+	char* log_level; 
+	if(config_has_property(config,"LogLevel"))
+	 	log_level = config_get_string_value(config, "LogLevel");		
+	else 
+		log_level = "INFO";		
+
+	logger_initialize(log_file, "personaje.sh", log_level, 1);
+
 
 	//obtenemos una instancia del logger
 	self->logger = logger_new_instance();
+
 
 	int vidas = config_get_int_value(config, "vidas");
 	self->vidas_iniciales = vidas;
 	self->vidas = vidas;
 
 	self->ippuerto_orquestador = string_duplicate(config_get_string_value(config, "orquestador"));
-
 
 	int cantidad_niveles = config_get_int_value(config, "niveles");
 	char** nombres_niveles = config_get_array_value(config, "planDeNiveles");
@@ -67,6 +78,9 @@ t_personaje* personaje_crear(char* config_path){
 		list_add(niveles, nivel);
 	}
 	self->niveles = niveles;
+
+	logger_info(self->logger, "Log File:%s", log_file);
+	logger_info(self->logger, "Log Level:%s", log_level);
 
 	//liberamos recursos
 	config_destroy(config);
