0a9c82f63cbacdbef32fe51ea937dc3880d80e97
Andres Nagel <montsegur87@gmail.com>
2013-12-08 05:52:15 -0300

    Agrege koopa y el script para iniciarlo y configurarlo

---
+++ filesystem/koopa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Este shell script ejecuta koopa contra el directorio de GRASA especificado y otro directorio para comparacion.
+clear
+
+directorio_grasa="grasa" #El directorio donde esta montado grasa.
+koopa="koopa-2c2013/Release/./koopa" #El path a koopa.
+scripts="scripts.sh" #El path al archivo de scripts
+
+echo "Ejecutando koopa."
+echo "Directorio grasa: $directorio_grasa"
+echo "Koopa: $koopa"
+echo "Scripts: $scripts"
+
+$koopa $directorio_grasa $scripts
---
+++ filesystem/scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+touch archivo1.txt
+touch archivo2.txt
+touch archivo3.txt
+touch archivo4.txt
+touch archivo5.txt
+
+rm archivo2.txt
+rm archivo3.txt
+
+mkdir dir1
+mkdir dir2
+mkdir dir3
+
+rmdir dir2
+rmdir dir3
