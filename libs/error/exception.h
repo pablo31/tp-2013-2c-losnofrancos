468197ae75cb333ac2dcc5419d478474ed05057f
pablo31 <psfutn@gmail.com>
2013-12-10 16:37:47 -0300

    Nada que les interese

--- libs/error/exception.h
+++ libs/error/exception.h
@@ -14,8 +14,25 @@
 #ifndef EXCEPTION_H_
 #define EXCEPTION_H_
 
+
+	//thread local storage variable definition
+	#ifndef _PTHREAD_H
+		#define tls_var
+	#else
+		#define tls_var __thread
+	#endif
+
+
 	/***************************************************************
-	 * General exception handling
+	 * Exception handling thread-local variables
+	 ***************************************************************/
+
+	tls_var process_status __try_ps;
+	tls_var int excno;
+
+
+	/***************************************************************
+	 * Exception handling public macros
 	 ***************************************************************/
 
 	//java-style macros
@@ -43,84 +60,24 @@
 		else
 
 
-	//predefined exceptions
+	/***************************************************************
+	 * Exception handling private macros
+	 ***************************************************************/
+
+	//try implementation macro
+	#define SAVE_PS \
+		excno = save_process_status(__try_ps)
+
+	//throw implementation macro
+	#define LOAD_PS(ex) \
+		set_process_status(__try_ps, ex)
+
+
+	/***************************************************************
+	 * Predefined exceptions
+	 ***************************************************************/
+
 	#define RETRY_EXCEPTION 1
 
 
-	#ifndef THREAD_H_
-
-	/***************************************************************
-	 * Mono-thread programs exception handling
-	 ***************************************************************/
-
-		//global variables
-		process_status __try_ps;
-		int excno;
-
-		//try implementation macro
-		#define SAVE_PS \
-			excno = save_process_status(__try_ps)
-
-		//throw implementation macro
-		#define LOAD_PS(ex) \
-			set_process_status(__try_ps, ex)
-
-	#else
-
-	/***************************************************************
-	 * Multi-thread programs exception handling
-	****************************************************************/
-
-		//global variables
-		tad_mutex __try_mutex = mutex_static;
-		int __try_init = 0;
-
-		//thread-local variables keys
-		tad_thread_key __try_key;
-		tad_thread_key __try_excno;
-
-		//aux macros
-		#define ptr_to_int(ptr) (int)(intptr_t)ptr
-		#define int_to_ptr(value) (void*)(intptr_t)value
-
-		//exception number implementation macro
-		#define excno ptr_to_int(thread_get_variable(__try_excno))
-		#define set_excno(value) thread_set_variable(__try_excno, int_to_ptr(value))
-
-		//try implementation macro
-		#define SAVE_PS \
-			SAVE_PS_1(__LINE__)
-		#define SAVE_PS_1(unique_id) \
-			SAVE_PS_2(unique_id)
-		#define SAVE_PS_2(unique_id) \
-			SAVE_PS_3(__try_ps ## unique_id)
-		#define SAVE_PS_3(ps) \
-			mutex_close(&__try_mutex); \
-			INIT_PS_THREAD_VARIABLES \
-			process_status ps; \
-			thread_set_variable(__try_key, &ps); \
-			mutex_open(&__try_mutex); \
-			set_excno(save_process_status(ps))
-
-		#define INIT_PS_THREAD_VARIABLES \
-			if(!__try_init){ \
-				__try_init = 1; \
-				__try_key = thread_create_variable(); \
-				__try_excno = thread_create_variable(); \
-			}
-
-		//throw implementation macro
-		#define LOAD_PS(ex) \
-			LOAD_PS_1(ex, __LINE__)
-		#define LOAD_PS_1(ex, unique_id) \
-			LOAD_PS_2(ex, unique_id)
-		#define LOAD_PS_2(ex, unique_id) \
-			LOAD_PS_3(ex, __throw_ps ## unique_id)
-		#define LOAD_PS_3(ex, ps) \
-			do{ \
-				process_status* ps = thread_get_variable(__try_key); \
-				set_process_status(*ps, ex); \
-			}while(0)
-
-	#endif /* ifndef THREAD_H_ */
 #endif /* ifndef EXCEPTION_H_ */
