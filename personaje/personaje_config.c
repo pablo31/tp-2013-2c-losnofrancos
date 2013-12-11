0f0e906ad86543f65bdc6b37745ca2a61fcfe6f4
pablo31 <psfutn@gmail.com>
2013-12-11 13:37:37 -0300

    Ya no se requiere el atributo Cantidad de Niveles en Personaje

--- personaje/personaje_config.c
+++ personaje/personaje_config.c
@@ -8,16 +8,17 @@
 
 #include "../libs/common/string.h"
 #include "../libs/common/config.h"
+#include "../libs/common.h"
 
 
 #include "personaje.h"
 
 static void liberar_niveles(char** niveles){
-	if (NULL == niveles)
+	if (null == niveles)
 		return;
 
 	uint i = 0;
-	while(niveles[i] != NULL){
+	while(niveles[i] != null){
 		free(niveles[i]);
 		i++;
 	}
@@ -33,10 +34,14 @@ t_personaje* personaje_crear(char* config_path){
 	t_config* config = config_create(config_path);
 	t_config* global_config = config_create("global.cfg");
 
+	//datos basicos del personaje
 	self->nombre = string_duplicate(config_get_string_value(config, "Nombre"));
 	self->simbolo = *config_get_string_value(config, "Simbolo");
+	int vidas = config_get_int_value(config, "Vidas");
+	self->vidas_iniciales = vidas;
+	self->vidas = vidas;
 
-	//cargamos los datos del logger
+	//datos del logger
 	char* log_file;
 	if(config_has_property(config,"LogFile")) log_file = config_get_string_value(config, "LogFile");
 	else log_file = string_from_format("%s.log", self->nombre);
@@ -48,18 +53,10 @@ t_personaje* personaje_crear(char* config_path){
 
 	//inicializamos el logger
 	logger_initialize(log_file, "personaje", log_level, 1);
-
-
-	//obtenemos una instancia del logger
 	self->logger = logger_new_instance();
+	var(logger, self->logger);
 
-
-	int vidas = config_get_int_value(config, "Vidas");
-	self->vidas_iniciales = vidas;
-	self->vidas = vidas;
-
-
-	//direccion de plataforma
+	//datos de plataforma
 	char* plataforma;
 	if(config_has_property(global_config, "Plataforma"))
 		plataforma = string_duplicate(config_get_string_value(global_config, "Plataforma"));
@@ -67,40 +64,40 @@ t_personaje* personaje_crear(char* config_path){
 		plataforma = string_duplicate(config_get_string_value(config, "Plataforma"));
 	self->ippuerto_orquestador = plataforma;
 
-
-	int cantidad_niveles = config_get_int_value(config, "Niveles");
-	char** nombres_niveles = config_get_array_value(config, "PlanDeNiveles");
-
+	//datos de niveles y objetivos
+	char** nombres_niveles = config_get_array_value(config, "Niveles");
 	t_list* niveles = list_create();
-	int i;
-	for(i = 0; i < cantidad_niveles; i++){
+	int i = 0;
+	char* nombre_nivel = nombres_niveles[i];
+
+	while(nombre_nivel != null){
 		alloc(nivel, t_nivel);
-		char* nombre_nivel = string_duplicate(nombres_niveles[i]);
-		nivel->nombre = nombre_nivel;
+		nivel->nombre = string_duplicate(nombre_nivel);
 		nivel->objetivos = list_create();
 
 		char* key_objetivos = string_from_format("Obj[%s]", nombre_nivel);
 		char* objetivos = config_get_string_value(config, key_objetivos);
 
-		logger_info(self->logger, "Nivel: %s Objetivos:%s", nivel->nombre, objetivos);
+		logger_info(logger, "Nivel: %s", nivel->nombre);
+		logger_info(logger, "Objetivos: %s", objetivos);
 		int ii = 0;
-		while(objetivos != NULL && '\0' != objetivos[ii]){
+		while(objetivos != null && objetivos[ii] != '\0'){
 			char* objetivo = string_from_format("%c" , objetivos[ii]);
 			list_add(nivel->objetivos, objetivo);
-
-			logger_info(self->logger, "Objetivo:%s", objetivo);
 			ii++;
 		}
 
 		free(key_objetivos);
 
 		list_add(niveles, nivel);
-	}
 
+		i++;
+		nombre_nivel = nombres_niveles[i];
+	}
 	self->niveles = niveles;
 
-	logger_info(self->logger, "Log File:%s", log_file);
-	logger_info(self->logger, "Log Level:%s", log_level);
+	logger_info(logger, "Log File:%s", log_file);
+	logger_info(logger, "Log Level:%s", log_level);
 
 	//liberamos recursos
 	config_destroy(config);
