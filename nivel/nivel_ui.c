7a6791cb2fe0ae85221318d15106ec2539e95b5c
pablo31 <psfutn@gmail.com>
2013-12-12 13:15:54 -0300

    Creo q arregle el leak de la GUI de Nivel

--- nivel/nivel_ui.c
+++ nivel/nivel_ui.c
@@ -18,7 +18,7 @@ private WINDOW* mainwin;
 private int rows, cols;
 private tad_logger* logger;
 private tad_mutex* semaforo_dibujado;
-private bool gui_inicializada = false;
+private int gui_inicializada = 0;
 
 
 private void nivel_gui_get_term_size(int as_out rows, int as_out cols){
@@ -210,7 +210,7 @@ void nivel_gui_inicializar(){
 
 	semaforo_dibujado = mutex_create();
 
-	gui_inicializada = true;
+	gui_inicializada = 1;
 }
 
 private void nivel_gui_draw_char(char ch, vector2 pos, int color_pair){
@@ -279,5 +279,5 @@ void nivel_gui_terminar(){
 	mutex_dispose(semaforo_dibujado);
 	logger_info(logger, "Finalizada");
 	logger_dispose_instance(logger);
-	gui_inicializada = false;
+	gui_inicializada = 0;
 }
